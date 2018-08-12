f1a1e475593390b3bd5c58cb0da30c4212b99832
Anastasiia Chepka <abratochkina@gmail.com>
2018-08-12 14:52:22 +0300

    Add cart badge to header to display number of products in cart, split layout for control buttons at cart page depending on the size of screen

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,26 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { BaseComponent } from '@app/shared/components/base/base.component';
+
+import { Store } from '@ngrx/store';
+import * as fromRoot from '@app/shared/reducers';
+
+import { filter, takeUntil } from 'rxjs/operators';
+import { CartItem } from '@app/shared/models/cart';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent extends BaseComponent implements OnInit {
   isSidebarOpen: boolean = false;
   burgerMenuPath = "/assets/images/menu_mob.svg";
+  cartItems: Array<CartItem>;
+  numberOfProductsInCart: number;
 
   constructor(
-    private router: Router
-  ) { }
-
-  ngOnInit() {
+    private router: Router,
+    private store: Store<fromRoot.State>
+  ) { 
+    super();
   }
 
-  public toggleSidebar() {
+  ngOnInit() {
+    this.getNumberOfProductsInCart();
+  }
+
+  getNumberOfProductsInCart() {
+    this.store.select(state => state.cart)
+      .pipe(
+        filter(_ => !!_.cartItems),
+        takeUntil(this.unsubscribe$)
+      ).subscribe(cartState => {
+        this.cartItems = cartState.cartItems.data;
+        this.numberOfProductsInCart = this.cartItems.reduce((sum: number, item: CartItem) => {
+          return sum + item.quantity;
+        }, 0)
+      },
+      err => {
+        console.log(err);
+      })
+
+  }
+
+  toggleSidebar() {
     this.isSidebarOpen = !this.isSidebarOpen;
   }
 
-  public openLink(e: MouseEvent, linkUrl: string) {
+  openLink(e: MouseEvent, linkUrl: string) {
     e.preventDefault();
     this.isSidebarOpen = false;
     this.router.navigateByUrl(linkUrl);
